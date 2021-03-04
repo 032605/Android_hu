03da65871b3bd8823b129681fb70e0de920f8162
032605 <032605@naver.com>
2021-03-04 15:38:57 +0900

    day1

---
+++ app/src/main/java/com/example/firstproject/RelavtiveActivity.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelavtiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
